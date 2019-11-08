fdb94b0eff4607daa69ff7002bf3c9d3028ca8b5
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2019-11-08 09:46:46 +0000

    Removed all ChemShell files

--- chemsh/tutorial/samples/ionic_liquids/designing_files/CuSO4_H2O/new_runs/2_cuso4_partition/view.py
+++
@@ -1,8 +0,0 @@
-from chemsh.io.converter import convert_to_atoms
-from ase.visualize import view
-
-
-
-cluster = Fragment(coords='cuso4_solv_regions.pun', connmode='ionic')
-
-view(convert_to_atoms(cluster))
--- chemsh/tutorial/samples/ionic_liquids/designing_files/CuSO4_H2O/new_runs/view.py
+++
@@ -1,8 +0,0 @@
-from chemsh.io.converter import convert_to_atoms
-from ase.visualize import view
-
-
-
-cluster = Fragment(coords='<<filename>>', connmode='ionic')
-
-view(convert_to_atoms(cluster))
