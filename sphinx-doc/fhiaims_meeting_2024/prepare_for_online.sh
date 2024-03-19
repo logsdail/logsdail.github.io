90e60c2777dac30bfaa662cecceb19900b108d14
ovanv <vanvureno@cardiff.ac.uk>
2024-03-19 11:46:12 +0000

    added 2024 aims meeting

---
+++ sphinx-doc/fhiaims_meeting_2024/prepare_for_online.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf build/html/sources build/html/static
+mv build/html/_sources build/html/sources
+mv build/html/_static build/html/static
+sed -e "s/_static/static/g" -e "s/_sources/sources/g" build/html/index.html > temp
+mv temp build/html/index.html
+
+mv build/html/index.html ../../
+cp -r build/html/static ../../
+cp -r build/html/sources ../../
