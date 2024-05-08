bcf1e4fe3d14cadf200169f46e37b74a95f83175
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2024-05-08 23:49:06 +0100

    Bit more tidying of code/pages

--- sphinx-doc/fhiaims_meeting_2024/prepare_for_online.sh
+++ sphinx-doc/fhiaims_meeting_2024/prepare_for_online.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -rf build/html/sources build/html/static
+rm -rf build/html/images build/html/sources build/html/static
 mv build/html/_sources build/html/sources
 mv build/html/_static build/html/static
-sed -e "s/_static/static/g" -e "s/_sources/sources/g" build/html/index.html > temp
+mv build/html/_images build/html/images
+sed -e "s/_static/static/g" -e "s/_sources/sources/g" -e "s/_images/images/g" build/html/index.html > temp
 mv temp build/html/index.html
 
 #mv build/html/index.html ../../
