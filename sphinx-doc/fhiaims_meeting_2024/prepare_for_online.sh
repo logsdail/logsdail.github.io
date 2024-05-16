7bb9e02f5ec5a70219749f5bbaf3a4ec50c80ef9
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2024-05-16 12:04:56 +0100

    Added photos

--- sphinx-doc/fhiaims_meeting_2024/prepare_for_online.sh
+++ sphinx-doc/fhiaims_meeting_2024/prepare_for_online.sh
@@ -6,7 +6,7 @@ mv build/html/_static build/html/static
 mv build/html/_images build/html/images
 sed -e "s/_static/static/g" -e "s/_sources/sources/g" -e "s/_images/images/g" build/html/index.html > temp
 mv temp build/html/index.html
-cp Map2.png build/html/images
+cp Map2.png conference_photo.jpg build/html/images
 # This adds a nofollow, so the search engines don't track the content
 # I know the layout looks strange but that's what you need to do on OS X
 sed -i='' '6i\
